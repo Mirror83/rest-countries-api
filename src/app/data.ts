611d98a0a92586f33a0995a60c077c991916804b
Mirror83 <glenochieng045@gmail.com>
2024-07-26 01:53:00 +0300

    Added getCountry method, additional interfaces and retrieval from REST Countries API instead of localhost

--- src/app/data.ts
+++ src/app/data.ts
@@ -2,15 +2,69 @@ export const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
 export type Region = "Africa" | "America" | "Asia" | "Europe" | "Oceania";
 
 export interface CountrySummary {
+  alpha3Code: string;
   name: string;
   population: number;
   region: Region;
   capital: string;
+  flag: Flag;
 }
 
-export async function getCountries(): Promise<CountrySummary[]> {
-  const res = await fetch("http://localhost:5000/countries");
-  const countires = await res.json();
+export interface CountryDetails extends CountrySummary {
+  nativeName: string;
+  subRegion: string;
+  topLevelDomain: string;
+  currencies: Currency[];
+  languages: Language[];
+  borders: string[];
+}
+export interface Currency {
+  code: string;
+  name: string;
+  symbol: string;
+}
+
+export interface Flag {
+  alt?: string;
+  png: string;
+  svg: string;
+}
+
+export interface Language {
+  iso639_1: string;
+  iso639_2: string;
+  name: string;
+  nativeName: string;
+}
+
+export async function getAllCountries(
+  countryName?: string,
+  region?: Region
+): Promise<CountrySummary[]> {
+  const fields = "alpha3Code,name,population,region,flags,capital";
+
+  const url = countryName
+    ? `https://restcountries.com/v2/name/${countryName}`
+    : `https://restcountries.com/v2/all`;
+
+  const res = await fetch(`${url}?fields=${fields}`);
+  const countires: CountrySummary[] = await res.json();
+
+  if (region) {
+    return countires.filter((country) => country.region == region);
+  }
 
   return countires;
+}
+
+export async function getCountry(alpha3Code: string): Promise<CountryDetails> {
+  const fields =
+    "name,nativeName,population,region,flags,capital,subregion,languages,topLevelDomain,currencies,borders";
+  const res = await fetch(
+    `https://restcountries.com/v2/alpha/${alpha3Code}?fields=${fields}`
+  );
+
+  const countryDetails: CountryDetails = await res.json();
+
+  return countryDetails;
 }
