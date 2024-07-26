dc1f7adb249ae9355f10ae8434ae064713395156
Mirror83 <glenochieng045@gmail.com>
2024-07-26 22:11:40 +0300

    Added icon to button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { ArrowLeftIcon, BackwardIcon } from "@heroicons/react/16/solid";
 import { useRouter } from "next/navigation";
 
 export function BackButton() {
-  "use client";
   const { back } = useRouter();
   return (
     <button
-      className="mb-16 bg-container dark:bg-container-dark px-8 py-2"
+      className="mb-16 bg-container dark:bg-container-dark px-8 py-2 shadow-md rounded-md"
       onClick={back}
     >
-      Back
+      <div className="flex gap-2 items-center">
+        <ArrowLeftIcon height={20} />
+        <span>Back</span>
+      </div>
     </button>
   );
 }
