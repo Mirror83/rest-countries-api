c73e997cac27efad46b455cf60c169b7869cc9af
Mirror83 <glenochieng045@gmail.com>
2024-07-26 09:06:27 +0300

    Imported api response saved in data.json and modified some interface types

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,22 +1,23 @@
 export const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
 export type Region = "Africa" | "America" | "Asia" | "Europe" | "Oceania";
+import mockData from "@/data.json";
 
 export interface CountrySummary {
   alpha3Code: string;
   name: string;
   population: number;
-  region: Region;
-  capital: string;
-  flag: Flag;
+  region: Region | string;
+  capital?: string;
+  flags: Flags;
 }
 
 export interface CountryDetails extends CountrySummary {
   nativeName: string;
-  subRegion: string;
-  topLevelDomain: string;
-  currencies: Currency[];
+  subregion: string;
+  topLevelDomain: string[];
+  currencies?: Currency[];
   languages: Language[];
-  borders: string[];
+  borders?: string[];
 }
 export interface Currency {
   code: string;
@@ -24,47 +25,74 @@ export interface Currency {
   symbol: string;
 }
 
-export interface Flag {
+export interface Flags {
   alt?: string;
   png: string;
   svg: string;
 }
 
 export interface Language {
-  iso639_1: string;
-  iso639_2: string;
   name: string;
-  nativeName: string;
+  nativeName?: string;
 }
 
 export async function getAllCountries(
   countryName?: string,
   region?: Region
 ): Promise<CountrySummary[]> {
-  const fields = "alpha3Code,name,population,region,flags,capital";
+  try {
+    const fields = "alpha3Code,name,population,region,flags,capital";
 
-  const url = countryName
-    ? `https://restcountries.com/v2/name/${countryName}`
-    : `https://restcountries.com/v2/all`;
+    const url = countryName
+      ? `https://restcountries.com/v2/name/${countryName}`
+      : `https://restcountries.com/v2/all`;
 
-  const res = await fetch(`${url}?fields=${fields}`);
-  const countires: CountrySummary[] = await res.json();
+    const res = await fetch(`${url}?fields=${fields}`);
+    const countires: CountrySummary[] = await res.json();
 
-  if (region) {
-    return countires.filter((country) => country.region == region);
+    if (region) {
+      return countires.filter((country) => country.region == region);
+    }
+
+    return countires;
+  } catch (err) {
+    console.error(err);
   }
 
-  return countires;
+  return mockData.countries.map((country) => {
+    return {
+      alpha3Code: country.alpha3Code,
+      name: country.name,
+      flags: country.flags,
+      population: country.population,
+      region: country.region,
+      capital: country.capital,
+    };
+  });
 }
 
 export async function getCountry(alpha3Code: string): Promise<CountryDetails> {
-  const fields =
-    "name,nativeName,population,region,flags,capital,subregion,languages,topLevelDomain,currencies,borders";
-  const res = await fetch(
-    `https://restcountries.com/v2/alpha/${alpha3Code}?fields=${fields}`
-  );
+  try {
+    const fields =
+      "name,nativeName,population,region,flags,capital,subregion,languages,topLevelDomain,currencies,borders";
+    const res = await fetch(
+      `https://restcountries.com/v2/alpha/${alpha3Code}?fields=${fields}`
+    );
 
-  const countryDetails: CountryDetails = await res.json();
+    const countryDetails: CountryDetails = await res.json();
 
-  return countryDetails;
+    return countryDetails;
+  } catch (err) {
+    console.error(err);
+  }
+
+  const country = mockData.countries.find((country) => {
+    country.alpha3Code == alpha3Code;
+  });
+
+  if (!country) {
+    throw new Error("Invalid country code.");
+  }
+
+  return country;
 }
