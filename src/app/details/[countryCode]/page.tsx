e41489d0c094f02e49ff91fa63887d13aae6b135
Mirror83 <glenochieng045@gmail.com>
2024-07-26 22:12:08 +0300

    Added desktop styling

--- src/app/details/[countryCode]/page.tsx
+++ src/app/details/[countryCode]/page.tsx
@@ -12,10 +12,13 @@ export default async function Page({
   const details = await getCountry(params.countryCode);
 
   return (
-    <div className="flex flex-col items-center pb-8 pt-12 px-8 md:p-0">
-      <div>
+    <div className="flex flex-col items-center pb-12 pt-12 px-8">
+      <div className="lg:w-11/12">
         <BackButton />
-        <DetailCard details={details} />
+        <DetailCard
+          details={details}
+          className="lg:grid grid-cols-2 gap-16 items-center"
+        />
       </div>
     </div>
   );
@@ -31,56 +34,61 @@ function DetailCard({ details, className }: DetailCardProps) {
 
   return (
     <div className={cn(className)}>
-      {/** Flag */}
-      {/* <div className="h-[200px] w-[300px] bg-yellow-800 rounded-md"></div> */}
       <img
-        width={300}
-        height={200}
-        src={details.flags.png}
+        width={600}
+        height={500}
+        src={details.flags.svg}
         alt={details.flags.alt || `Flag of the country ${details.name}`}
       />
-      <div className="my-8">
-        <strong className="text-2xl font-extrabold">{details.name}</strong>
-      </div>
-      <div className="mb-8">
-        <InfoRow name={"Native name"} value={details.nativeName} />
-        <InfoRow name={"Population"} value={details.population} />
-        <InfoRow name={"Region"} value={details.region} />
-        <InfoRow name={"Sub Region"} value={details.subregion} />
-        <InfoRow name={"Capital"} value={details.capital} />
-      </div>
-
-      <div className="mb-8">
-        <InfoRow
-          name={"Top Level Domain"}
-          value={details.topLevelDomain.join(", ")}
-        />
-        <InfoRow
-          name={"Currencies"}
-          value={details.currencies
-            ?.map((currency) => currency.name)
-            .join(", ")}
-        />
-        <InfoRow
-          name={"Languages"}
-          value={details.languages?.map((language) => language.name).join(", ")}
-        />
-      </div>
-
       <div>
-        <p className="font-bold mb-4">Border Countries:</p>
+        <div className="my-8">
+          <strong className="text-2xl font-extrabold">{details.name}</strong>
+        </div>
 
-        <div className="flex flex-wrap gap-2">
-          {borders.map((border) => (
-            <Link
-              key={border.alpha3Code}
-              href={`/details/${border.alpha3Code}`}
-            >
-              <div className="px-4 py-2 bg-container dark:bg-container-dark">
-                {border.name}
-              </div>
-            </Link>
-          ))}
+        <div className="lg:grid md:grid-cols-2">
+          <div className="mb-8">
+            <InfoRow name={"Native name"} value={details.nativeName} />
+            <InfoRow name={"Population"} value={details.population} />
+            <InfoRow name={"Region"} value={details.region} />
+            <InfoRow name={"Sub Region"} value={details.subregion} />
+            <InfoRow name={"Capital"} value={details.capital} />
+          </div>
+
+          <div className="mb-8">
+            <InfoRow
+              name={"Top Level Domain"}
+              value={details.topLevelDomain.join(", ")}
+            />
+            <InfoRow
+              name={"Currencies"}
+              value={details.currencies
+                ?.map((currency) => currency.name)
+                .join(", ")}
+            />
+            <InfoRow
+              name={"Languages"}
+              value={details.languages
+                ?.map((language) => language.name)
+                .join(", ")}
+            />
+          </div>
+        </div>
+
+        <div>
+          <p className="font-bold mb-4">Border Countries:</p>
+
+          <div className="flex flex-wrap gap-2">
+            {borders.map((border) => (
+              <Link
+                key={border.alpha3Code}
+                href={`/details/${border.alpha3Code}`}
+              >
+                <div className="px-4 py-2 bg-container dark:bg-container-dark">
+                  {border.name}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
