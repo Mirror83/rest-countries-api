498abd2f0bf4d31faae44422a767aeb34a4fcf0b
Mirror83 <glenochieng045@gmail.com>
2024-07-26 22:05:35 +0300

    Moved getCurrentTheme into a useLayoutEffect to ensure it has access to the window object

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { MoonIcon } from "@heroicons/react/16/solid";
 import { SunIcon } from "@heroicons/react/24/outline";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 function Navbar() {
   return (
@@ -17,29 +17,33 @@ function Navbar() {
 }
 
 function ThemeToggler() {
-  function getCurrentTheme(): "dark" | "light" {
-    const osPrefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark" || (savedTheme === null && osPrefersDark)) {
-      return "dark";
-    } else {
-      return "light";
-    }
-  }
-
   function toggleTheme() {
-    setState((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   }
 
-  const [theme, setState] = useState(getCurrentTheme());
+  const [theme, setTheme] = useState("");
+
+  useLayoutEffect(() => {
+    function getCurrentTheme() {
+      const osPrefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      const savedTheme = localStorage.getItem("theme");
+      if (savedTheme === "dark" || (savedTheme === null && osPrefersDark)) {
+        return "dark";
+      } else {
+        return "light";
+      }
+    }
+
+    setTheme(getCurrentTheme());
+  }, []);
 
   useEffect(() => {
     const osPrefersDark = window.matchMedia(
       "(prefers-color-scheme: dark)"
     ).matches;
-    if (theme === "dark" || (theme === null && osPrefersDark)) {
+    if (theme === "dark" || (theme === "" && osPrefersDark)) {
       localStorage.setItem("theme", "dark");
       document.querySelector("html")?.classList.add("dark");
     } else {
