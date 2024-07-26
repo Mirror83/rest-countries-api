d50573101d2ada73ab1a592d4107a7b73174d4ef
Mirror83 <glenochieng045@gmail.com>
2024-07-26 23:46:22 +0300

    Modified Error to take extra arguments

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import Link from "next/link";
+import { useEffect } from "react";
 
-export default function Page() {
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.log(error);
+  }, []);
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex-auto flex items-center justify-center">
       <div className="flex flex-col items-center">
-        <strong>Something went wrong</strong>
-        <Link href={"/"}>Home</Link>
+        <strong className="text-xl">{error.message}</strong>
+        <div className="flex gap-4 items-center my-4">
+          <Link href={"/"} className="underline">
+            Back Home
+          </Link>
+        </div>
       </div>
     </div>
   );
