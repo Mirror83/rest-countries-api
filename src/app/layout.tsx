579e1706d65d40cb79e61fb19e4a87c1cb732a78
Mirror83 <glenochieng045@gmail.com>
2024-07-26 19:29:05 +0300

    Moved colour utility class declarations to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { cn } from "@/lib/utils";
 
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className="bg-surface dark:bg-surface-dark text-on-container dark:text-on-container-dark"
-    >
-      <body className={nunitoSans.className}>
+    <html lang="en">
+      <body
+        className={cn(
+          nunitoSans.className,
+          "bg-surface dark:bg-surface-dark text-on-container dark:text-on-container-dark"
+        )}
+      >
         <Navbar />
         <main>{children}</main>
       </body>
