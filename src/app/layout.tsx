7e65a37efd91aaec8bf017806edfb70e1e435543
Mirror83 <glenochieng045@gmail.com>
2024-07-26 11:39:40 +0300

    Changed font to Nunito Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
+const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Where in the world?",
@@ -20,7 +20,7 @@ export default function RootLayout({
       lang="en"
       className="bg-surface dark:bg-surface-dark text-on-container dark:text-on-container-dark"
     >
-      <body className={inter.className}>
+      <body className={nunitoSans.className}>
         <div className="flex flex-row flex-wrap justify-between items-baseline bg-container dark:bg-container-dark shadow p-8">
           <Link href="/">
             <h1 className="font-bold">Where in the World?</h1>
