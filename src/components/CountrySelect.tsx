b2299218d9f57fb0701222e703c440db46016cd5
Mirror83 <glenochieng045@gmail.com>
2024-07-26 15:28:08 +0300

    Implemented the select's resopnsibilites with url-based search by region

--- src/components/CountrySelect.tsx
+++ src/components/CountrySelect.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { regions } from "../app/data";
 
 export function CountrySelect() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSelect(region: string) {
+    const params = new URLSearchParams(searchParams);
+    if (region) {
+      params.set("region", region);
+    } else {
+      params.delete("region");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <select
       className="bg-container dark:bg-container-dark p-4 rounded-md"
-      onChange={(e) => console.log(e.target.value)}
-      defaultValue={""}
+      onChange={(e) => handleSelect(e.target.value)}
+      defaultValue={searchParams?.get("region")?.toString()}
     >
       <option value="">Filter by region</option>
       {regions.map((region) => (
