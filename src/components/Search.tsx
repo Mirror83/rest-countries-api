2dc549f7ed94dd6a6031f896665394b168ee20bb
Mirror83 <glenochieng045@gmail.com>
2024-07-26 19:30:50 +0300

    Added icon and changed bg-color for light mode and removed unnecessary wrapping div

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 export default function Search() {
@@ -18,18 +19,16 @@ export default function Search() {
     replace(`${pathname}?${urlSearchParams.toString()}`);
   }
   return (
-    <div className="flex flex-wrap my-8">
-      <div className="bg-dark-gray dark:bg-container-dark flex items-center p-4 gap-4 w-full mb-8 rounded-md">
-        <p className="font-bold">Icon</p>
-        <input
-          className="bg-dark-gray dark:bg-container-dark border-none outline-none w-full"
-          name="country"
-          type="search"
-          defaultValue={searchParams.get("name")?.toString()}
-          placeholder="Search for a country..."
-          onChange={(e) => handleSearch(e.target.value)}
-        />
-      </div>
+    <div className="bg-container dark:bg-container-dark shadow-md flex items-center p-4 gap-2 w-full my-8 rounded-md">
+      <MagnifyingGlassIcon height={30} />
+      <input
+        className="bg-container dark:bg-container-dark border-none outline-none w-full"
+        name="country"
+        type="search"
+        defaultValue={searchParams.get("name")?.toString()}
+        placeholder="Search for a country..."
+        onChange={(e) => handleSearch(e.target.value)}
+      />
     </div>
   );
 }
