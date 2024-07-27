01826aad312934be0170d556281a55e6eb408a37
Mirror83 <glenochieng045@gmail.com>
2024-07-27 10:49:39 +0300

    Added debounncing to handleSearch function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(name: string) {
+  const handleSearch = useDebouncedCallback((name: string) => {
     const urlSearchParams = new URLSearchParams(searchParams);
     if (name) {
       urlSearchParams.set("name", name);
@@ -17,7 +18,8 @@ export default function Search() {
     }
 
     replace(`${pathname}?${urlSearchParams.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="bg-container dark:bg-container-dark shadow-md flex items-center p-4 gap-2 w-full my-8 rounded-md">
       <MagnifyingGlassIcon height={30} />
