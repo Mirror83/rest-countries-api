d1e23effb4bc8180cee3a8d2d9683142777e040a
Mirror83 <glenochieng045@gmail.com>
2024-07-27 11:28:18 +0300

    Added className prop

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
-export default function Search() {
+export default function Search({ className }: { className?: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -21,7 +22,12 @@ export default function Search() {
   }, 300);
 
   return (
-    <div className="bg-container dark:bg-container-dark shadow-md flex items-center p-4 gap-2 w-full my-8 rounded-md">
+    <div
+      className={cn(
+        "bg-container dark:bg-container-dark shadow-md flex items-center p-4 gap-2 w-full my-8 rounded-md",
+        className
+      )}
+    >
       <MagnifyingGlassIcon height={30} />
       <input
         className="bg-container dark:bg-container-dark border-none outline-none w-full"
