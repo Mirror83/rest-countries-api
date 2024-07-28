6fa86c07718c48ad761c7ab8b98b932cd76103db
Mirror83 <glenochieng045@gmail.com>
2024-07-28 23:05:21 +0300

    Removed flexbox div from button and made the button itself a flex element

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,13 +7,11 @@ export function BackButton() {
   const { back } = useRouter();
   return (
     <button
-      className="mb-8 bg-container dark:bg-container-dark px-8 py-2 shadow-md rounded-md"
+      className="flex gap-2 items-center mb-8 bg-container dark:bg-container-dark px-8 py-2 shadow-md rounded-md"
       onClick={back}
     >
-      <div className="flex gap-2 items-center">
-        <ArrowLeftIcon height={20} />
-        <span>Back</span>
-      </div>
+      <ArrowLeftIcon height={20} />
+      Back
     </button>
   );
 }
