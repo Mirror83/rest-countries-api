8df7add72a6dcafc03634dd9a795703e33846642
Mirror83 <glenochieng045@gmail.com>
2024-07-28 14:05:54 +0300

    Added an arrow-down "icon" to CountrySelect

--- src/components/CountrySelect.tsx
+++ src/components/CountrySelect.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { regions } from "../app/data";
 
@@ -19,17 +18,20 @@ export function CountrySelect() {
     replace(`${pathname}?${params.toString()}`);
   }
   return (
-    <select
-      className="bg-container dark:bg-container-dark p-4 rounded-md"
-      onChange={(e) => handleSelect(e.target.value)}
-      defaultValue={searchParams?.get("region")?.toString()}
-    >
-      <option value="">Filter by region</option>
-      {regions.map((region) => (
-        <option key={region} value={region}>
-          {region}
-        </option>
-      ))}
-    </select>
+    <div className="flex h-min gap-2 bg-container dark:bg-container-dark w-min p-4 rounded-md">
+      <select
+        className="appearance-none pe-8 hover:cursor-pointer bg-container dark:bg-container-dark"
+        onChange={(e) => handleSelect(e.target.value)}
+        defaultValue={searchParams?.get("region")?.toString()}
+      >
+        <option value="">Filter by region</option>
+        {regions.map((region) => (
+          <option key={region} value={region}>
+            {region}
+          </option>
+        ))}
+      </select>
+      <div>▼</div>
+    </div>
   );
 }
