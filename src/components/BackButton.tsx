83f56f7eea9fbda83b73b5a977bab0e94998c905
Mirror83 <glenochieng045@gmail.com>
2024-07-28 14:04:45 +0300

    Updated bottom margin value

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -7,7 +7,7 @@ export function BackButton() {
   const { back } = useRouter();
   return (
     <button
-      className="mb-16 bg-container dark:bg-container-dark px-8 py-2 shadow-md rounded-md"
+      className="mb-8 bg-container dark:bg-container-dark px-8 py-2 shadow-md rounded-md"
       onClick={back}
     >
       <div className="flex gap-2 items-center">
