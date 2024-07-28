7c7316b8ba3c8c06d014ca833e311d98b1576cbf
Mirror83 <glenochieng045@gmail.com>
2024-07-28 14:01:15 +0300

    Added Suspense boundary for CountrySummaries and modified styling of some elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { CountrySelect } from "../components/CountrySelect";
 import { getAllCountries, CountrySummary, Region } from "./data";
 import InfoRow from "@/components/InfoRow";
 import Search from "@/components/Search";
+import { cn } from "@/lib/utils";
+import { Suspense } from "react";
+import Loading from "@/components/Loading";
 
 export default async function Home({
   searchParams,
@@ -18,17 +21,33 @@ export default async function Home({
   );
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-11/12">
-        <Search />
+    <div className="">
+      <div className="flex flex-wrap w-full justify-between items-center px-8">
+        <Search className="md:max-w-[500px]" />
         <CountrySelect />
       </div>
+      <Suspense fallback={<Loading />}>
+        <CountrySummaries
+          summaries={summaries}
+          className="flex w-full flex-wrap gap-8 py-8 px-8 justify-center lg:grid-cols-4"
+        />
+      </Suspense>
+    </div>
+  );
+}
 
-      <div className="flex flex-1 flex-wrap gap-8 justify-center my-8">
-        {summaries.map((summary) => (
-          <CountrySummaryCard key={summary.name} {...summary} />
-        ))}
-      </div>
+function CountrySummaries({
+  summaries,
+  className,
+}: {
+  summaries: CountrySummary[];
+  className?: string;
+}) {
+  return (
+    <div className={cn(className)}>
+      {summaries.map((summary) => (
+        <CountrySummaryCard key={summary.name} {...summary} />
+      ))}
     </div>
   );
 }
@@ -44,7 +63,7 @@ function CountrySummaryCard({
   return (
     <div className="bg-container w-[300px] dark:bg-container-dark rounded-md shadow-md">
       <img
-        className="rounded-md h-[200px] object-cover"
+        className="rounded-md h-[200px] object-cover shadow-sm"
         width={300}
         height={200}
         src={flags.png}
