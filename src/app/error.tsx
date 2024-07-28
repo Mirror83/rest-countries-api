7ef8698ff3114580550edc49bdd9507e3a8ea0cb
Mirror83 <glenochieng045@gmail.com>
2024-07-28 17:16:34 +0300

    Added check for fetch error and added conditional ui with respect to it

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { FaceFrownIcon, WifiIcon } from "@heroicons/react/24/outline";
 import { useEffect } from "react";
 
 export default function Error({
@@ -12,10 +13,22 @@ export default function Error({
   useEffect(() => {
     console.log(error);
   }, []);
+
+  const isFetchError = error.message === "fetch failed";
+
   return (
     <div className="flex-auto flex items-center justify-center">
       <div className="flex flex-col items-center">
-        <strong className="text-xl">{error.message}</strong>
+        {isFetchError ? (
+          <WifiIcon height={60} className="mb-4" />
+        ) : (
+          <FaceFrownIcon height={60} className="mb-4" />
+        )}
+        <strong className="text-xl">
+          {isFetchError
+            ? "Check your internet connection and then try again."
+            : error.message}
+        </strong>
         <div className="flex gap-4 items-center my-4">
           <a href={"/"} className="underline">
             Back Home
